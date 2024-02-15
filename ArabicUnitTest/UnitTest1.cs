2e7d1032e6a0dd67bee4431d6c3290b24471dac2
Pedro Javier Lora <javierpd45@gmail.com>
2024-02-14 22:38:46 -0400

    Tests created and ready to use

--- ArabicUnitTest/UnitTest1.cs
+++ ArabicUnitTest/UnitTest1.cs
@@ -8,9 +8,87 @@ namespace ArabicUnitTest
         }
 
         [Test]
-        public void Test1()
+        public void Convert_1_to_I()
         {
-            Assert.Pass();
+            var convert = new ConversorNum();
+            var num = 1;
+
+            var result = convert.ConvertArabicRoman(num);
+
+            Assert.That(result, Is.EqualTo("I"));
+        }
+
+        [Test]
+        public void Convert_3999_to_MMMCMXCIX()
+        {
+            var convert = new ConversorNum();
+            var num = 3999;
+
+            var result = convert.ConvertArabicRoman(num);
+
+            Assert.That(result, Is.EqualTo("MMMCMXCIX"));
+        }
+
+        [Test]
+        public void Convert_500_to_D()
+        {
+            var convert = new ConversorNum();
+            var num = 500;
+
+            var result = convert.ConvertArabicRoman(num);
+
+            Assert.That(result, Is.EqualTo("D"));
+        }
+
+        [Test]
+        public void Convert_1234_to_MCCXXXIV()
+        {
+            var convert = new ConversorNum();
+            var num = 1234;
+
+            var result = convert.ConvertArabicRoman(num);
+
+            Assert.That(result, Is.EqualTo("MCCXXXIV"));
+        }
+
+        [Test]
+        public void Convert_0_throwException()
+        {
+            var convert = new ConversorNum();
+            var num = 0;
+
+            
+
+            try
+            {
+                var result = convert.ConvertArabicRoman(num);
+            }
+            catch (Exception ex)
+            {
+
+                Assert.That(ex.Message, Is.EqualTo("El numero debe estar entre 1 y 3999")); ;
+            }
+            
+        }
+
+        [Test]
+        public void Convert_5000_throwException()
+        {
+            var convert = new ConversorNum();
+            var num = 5000;
+
+
+
+            try
+            {
+                var result = convert.ConvertArabicRoman(num);
+            }
+            catch (Exception ex)
+            {
+
+                Assert.That(ex.Message, Is.EqualTo("El numero debe estar entre 1 y 3999")); ;
+            }
+
         }
     }
 }
